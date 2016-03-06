7963341e8d8f1c0ba71a341ef42738efbf7a2367
Jordan Harmon <Jordan Harmon>
2016-03-05 17:30:31 -0700

    welcome

--- src/main/java/sessions/Login.java
+++ src/main/java/sessions/Login.java
@@ -64,7 +64,7 @@ public class Login extends HttpServlet {
                 break;
             } if (valid == true) {
             request.getSession().setAttribute("username", username);
-            response.sendRedirect("landing.jsp");
+            response.sendRedirect("/landing.jsp");
             
             } else {
                 String message = "That is an invalid login";
