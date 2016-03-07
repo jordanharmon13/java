e6e538f47e8055d7ee2eda5758c73bc5fc453842
Jordan Harmon <Jordan Harmon>
2016-03-07 10:36:57 -0700

    clean and build

--- src/main/java/sessions/index.php
+++ src/main/java/sessions/index.php
@@ -5,9 +5,8 @@ set_time_limit(0);
 ini_set('default_socket_timeout', 300);
 session_start();
 
-CLIENT ID	8e92bafbcdcc4c849fdca959b0daba81
-CLIENT SECRET	024355aaf3d34ba995cdd7dcde5a6bef
-WEBSITE URL	http://java-jordanharmon.rhcloud.com/
-REDIRECT URI	http://java-jordanharmon.rhcloud.com/
-SUPPORT EMAIL	har12128@byui.edu
+define("clientID", '8e92bafbcdcc4c849fdca959b0daba81');
+define("clientSecret", '024355aaf3d34ba995cdd7dcde5a6bef');
+define("websiteURI", 'http://java-jordanharmon.rhcloud.com/');
+define("imageDirectory", 'pics/');
 ?>
