7b40279af96a0f7d546cf65b9adc395a0ab45c76
Jordan Harmon <Jordan Harmon>
2016-03-07 10:59:29 -0700

    change

--- src/main/webapp/index.php
+++ src/main/webapp/index.php
@@ -7,12 +7,12 @@ session_start();
 
 define("clientID", '8e92bafbcdcc4c849fdca959b0daba81');
 define("clientSecret", '024355aaf3d34ba995cdd7dcde5a6bef');
-define("websiteURI", 'http://java-jordanharmon.rhcloud.com/');
+define("redirectURI", 'http://java-jordanharmon.rhcloud.com/');
 define("imageDirectory", 'pics/');
 ?>
 <!DOCTYPE html>
 <html>
 <body>
-	<a href="https://api.instagram.com/oauth/authorize/?client_id=<?php echo clientID; ?>&redirect_url=<?php echo websiteURI; ?>&response_type=code">Login</a>
+	<a href="https://api.instagram.com/oauth/authorize/?client_id=8e92bafbcdcc4c849fdca959b0daba81&redirect_url=http://java-jordanharmon.rhcloud.com/&response_type=code">Login</a>
 </body>
 </html>
