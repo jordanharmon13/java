00eeac1d3c0a196f8c918a874e126bc15220aceb
Jordan Harmon <Jordan Harmon>
2016-03-22 11:43:32 -0600

    changes

--- src/main/java/instagram/instaOperations.java
+++ src/main/java/instagram/instaOperations.java
@@ -33,7 +33,7 @@ public class instaOperations extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            String instagramInfo = request.getParameter("reponse_type");
+            String instagramInfo = request.getParameter("code");
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
