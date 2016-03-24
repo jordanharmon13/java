ced118ac7b554545d6c08e986664285b057d7806
Jordan Harmon <Jordan Harmon>
2016-03-24 13:52:07 -0600

    changes

--- src/main/java/instagram/instaAPI.java
+++ src/main/java/instagram/instaAPI.java
@@ -7,7 +7,6 @@ package instagram;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import static java.lang.System.out;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
